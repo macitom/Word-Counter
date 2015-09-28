b7fb021d7facd1174735703b43537db7b78e1470
macitom <mactomas@gmail.com>
2015-09-28 19:32:58 +0100

    Project Setup

---
+++ WordCounter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("There are no words in this sentence.");
+            }
+            else
+            {
+                var words = (args[0].ToLower().GetWords()).GroupBy(w => w).ToDictionary(g => g.Key, g => g.Count());
+                Console.WriteLine(string.Join("\r\n", words.Select(w => w.Key + " - " + w.Value)));
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ WordCounter/TextParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace WordCounter
+{
+    public static class TextParser
+    {
+        public static IEnumerable<string> GetWords(this string text)
+        {
+            var word = new StringBuilder();
+            foreach (var c in text)
+            {
+                if (char.IsPunctuation(c) || char.IsWhiteSpace(c) || char.IsSeparator(c))
+                {
+                    if (word.Length > 0)
+                    {
+                        yield return word.ToString();
+                        word.Clear();
+                    }
+                }
+                else
+                {
+                    word.Append(c);
+                }
+            }
+            if (word.Length > 0)
+            {
+                yield return word.ToString();
+            }
+        }
+    }
+}
